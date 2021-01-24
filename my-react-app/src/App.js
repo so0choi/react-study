244c14c42dc7473917aa88ea4e42405485a6517a
so0choi <simc2644@gmail.com>
2021-01-25 00:00:52 +0900

    03. Add InputSample.js

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 
-import Counter from "./Counter";
+import InputSample from "./InputSample";
 
 function App() {
   return (
     <>
-      <Counter></Counter>
+      <InputSample></InputSample>
     </>
   );
 }
